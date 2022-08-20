84afa17c58c42a90efb2c96e4cad03cb9f1ac0e2
Arlet <shulga@niuitmo.ru>
2022-08-20 23:16:50 +0300

    Create base document

---
+++ web/lab1/index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Главная</title>
+    </head>
+    <body>
+        <div id="preview">
+            <p>Шульга Артём Игоревич P32XX1
+            <p>Вариант - <b>1819</b>
+        </div>
+        <div id="form">
+            <p>*Картинка*
+            <form name="coords" method="POST" action="point_checker.php">
+                <p>X:
+                <?php
+                    $template = "<input type=\"radio\" name=\"x\" value=\"%d\">";
+
+                    $min_value = -5;
+                    $max_value = 3;
+
+                    for($i=$min_value;$i<$max_value+1;$i++) {
+                        echo "$i:";
+                        printf($template, $i);
+                    }
+                ?>
+                <p>Y:
+                <input type="text" name="y">
+                <p>R:
+                <select>
+                    <?php
+                        $template = "<option>%d</option>";
+
+                        $min_value = 1;
+                        $max_value = 5;
+
+                        for($i = $min_value; $i<$max_value+1;$i++)
+                            printf($template, $i);
+                    ?>
+                </select>
+                <input type="submit">
+            </form>
+        </div>
+    </body>
+</html>
