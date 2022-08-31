ec0ffcadbe98fdb582a46165498416e990c81c2a
Arlet <shulga@niuitmo.ru>
2022-08-31 22:02:31 +0300

    Add new functions for send only coordinates

---
+++ web/lab1/coords_sender.js
@@ -0,0 +1,19 @@
+function sendCoordinates () {
+    let x = document.forms["coords"]["x"].value;
+    let y = document.forms["coords"]["y"].value.replace(',','.').replace(' ',"");
+    let r = document.forms["coords"]["r"].value;
+
+    if (!validate(x, y, r)) {
+        return;
+    }
+
+    sendRequest("POST", "point_handler.php", createArgs(x, y, r), handleRequest);
+}
+
+function createArgs (x, y, r) {
+    return "x="+x+"&y="+y+"n&r="+r;
+}
+
+function handleRequest (httpRequest) {
+    alert(httpRequest.responseText); // поменять на изменение таблицы
+}
