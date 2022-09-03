1316e71b7c5c2ce78a0ba06b4d6a68f0cad5f73f
Arlet <shulga@niuitmo.ru>
2022-09-03 20:19:41 +0300

    rename files

---
+++ web/lab1/point-handler.php
@@ -0,0 +1,27 @@
+<?php
+
+$startTime = hrtime(true);
+
+require "utils.php";
+require "hit-checker.php";
+require "data-validator.php";
+
+$x = preprocessValue($_POST["x"]);
+$y = preprocessValue($_POST["y"]);
+$r = preprocessValue($_POST["r"]);
+
+$dataIsCorrect = isArgumentsAreNumbers($x, $y, $r) && checkX($x) && checkY($y) && checkR($r);
+
+echo date(DATE_ATOM, time()) . " | " . changeCommaToPoint($x) . " | " . changeCommaToPoint($y) . " | " . changeCommaToPoint($r) . " | ";
+
+if (!$dataIsCorrect) {
+    echo "Ошибка ввода данных";
+} else if (isHit($x, $y, $r)) {
+    echo "Попал";
+} else {
+    echo "Не попал";
+}
+
+$interval = changePointToComma(round((hrtime(true) - $startTime) / (10 ** 6), 3)) . " ms";
+
+echo " | " . $interval;
