5732756f4ba0cb0cc1bbdd8db01f7e55c7720882
Arlet <shulga@niuitmo.ru>
2022-09-03 20:13:52 +0300

    Remove variables cast to int

--- web/lab1/hit_checker.php
+++ web/lab1/hit_checker.php
@@ -1,5 +1,5 @@
 <?php
-function isHit(int $x, int $y, int $r) : bool
+function isHit($x, $y, $r) : bool
 {
     if ($x == 0 || $y == 0) {
         return checkAxes($x, $y, $r);
@@ -14,27 +14,27 @@ function isHit(int $x, int $y, int $r) : bool
     }
 }
 
-function checkAxes(int $x, int $y, int $r) : bool
+function checkAxes($x, $y, $r) : bool
 {
     return $x <= $r && $x >= -$r && $y <= $r && $y >= -$r;
 }
 
-function isFirstQuarter(int $x, int $y, int $r) : bool
+function isFirstQuarter($x, $y, $r) : bool
 {
     return $x ** 2 + $y ** 2 <= $r ** 2;
 }
 
-function isSecondQuarter(int $x, int $y, int $r) : bool
+function isSecondQuarter($x, $y, $r) : bool
 {
     return $y <= $x + $r / 2;
 }
 
-function isThirdQuarter(int $x, int $y, int $r) : bool
+function isThirdQuarter($x, $y, $r) : bool
 {
     return abs(+$y) <= $r && abs(+$x) <= $r;
 }
 
-function isFourthQuarter(int $x, int $y, int $r) : bool
+function isFourthQuarter($x, $y, $r) : bool
 {
     return false;
 }
