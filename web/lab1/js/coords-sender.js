295a7e0c7864a926844ed977a3d4ab29cb5eff29
Arlet <shulga@niuitmo.ru>
2022-09-05 00:16:22 +0300

    Add JQuery using for ajax

--- web/lab1/js/coords-sender.js
+++ web/lab1/js/coords-sender.js
@@ -1,19 +1,46 @@
 function sendCoordinates () {
-    let x = document.forms["coords"]["x"].value;
-    let y = document.forms["coords"]["y"].value.replace(',','.').replace(' ',"");
-    let r = document.forms["coords"]["r"].value;
+    //let x = document.forms["coords"]["x"].value;
+    //let y = document.forms["coords"]["y"].value.replace(',','.').replace(' ',"");
+    //let r = document.forms["coords"]["r"].value;
 
-    if (!validate(x, y, r)) {
-        return;
-    }
+    let x = clearSpacesAndChangeCommaToPoint($('input[name="x"]:checked').val());
+    let y = clearSpacesAndChangeCommaToPoint($('input[name="y"]').val());
+    let r = clearSpacesAndChangeCommaToPoint($('select[name="r"]').val());
 
-    sendRequest("POST", "php/table.php", createArgs(x, y, r), handleRequest);
+    //if (!validate(x, y, r)) {
+    //    return;
+    //}
+
+    //sendRequest("POST", "php/table.php", createArgs(x, y, r), handleRequest);
+    $.ajax({
+        type: "POST",
+        url: "php/table.php",
+        data: {
+            'x': $('input[name="x"]:checked').val(),
+            'y': $('input[name="y"]').val(),
+            'r': $('select[name="r"]').val()
+        },
+        cache: false,
+        dataType: "html",
+        beforeSend: function () {
+            if (!validate(x, y, r)) {
+                return false;
+            }
+        },
+        success: function (response) {
+            handleRequest(response);
+        }
+    });
 }
 
 function createArgs (x, y, r) {
     return "x="+x+"&y="+y+"&r="+r;
 }
 
-function handleRequest (httpRequest) {
-    document.getElementById("receivingData").innerHTML = httpRequest.responseText;
+function handleRequest (response) {
+    document.getElementById("receivingData").innerHTML = response;
+}
+
+function clearSpacesAndChangeCommaToPoint(sendingValue) {
+    return (""+sendingValue).replace(',','.').replace(' ', "");
 }
