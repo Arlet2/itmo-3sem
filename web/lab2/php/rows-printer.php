c6e7bafb65bb3b083fa30e6ca4cee1492a36381d
Arlet <shulga@niuitmo.ru>
2022-10-02 00:21:44 +0300

    Recreate lab

--- web/lab2/php/rows-printer.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Rows.php";
-require_once "Row.php";
-function printRows($rows)
-{
-    foreach ($rows->getValues() as &$printingRow) {
-        echo $printingRow->getData() . "\n";
-    }    
-}
