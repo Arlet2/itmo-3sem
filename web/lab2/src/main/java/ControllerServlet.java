36d3b1a4a1d85a2400e85ca4bc9ee02fec2ba1fc
Arlet <shulga@niuitmo.ru>
2022-10-03 02:00:13 +0300

    Change mapping

--- web/lab2/src/main/java/ControllerServlet.java
+++ web/lab2/src/main/java/ControllerServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
-@WebServlet("/controller")
+@WebServlet("/")
 public class ControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
