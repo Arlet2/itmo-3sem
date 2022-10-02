97540b0aee67a83e32a6df3cf06f9ff9fa8e9568
Arlet <shulga@niuitmo.ru>
2022-10-02 19:53:32 +0300

    Create saver for data

---
+++ web/lab2/src/main/java/model/DataSaver.java
@@ -0,0 +1,28 @@
+package model;
+
+import data.Row;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class DataSaver {
+
+    public static final String SAVING_ATTRIBUTE_NAME = "rows";
+
+    public void saveData(HttpSession session, Row row) {
+        List<Row> rows;
+        try {
+            rows = (List<Row>) session.getAttribute(SAVING_ATTRIBUTE_NAME);
+        } catch (ClassCastException e) {
+            rows = new ArrayList<>();
+        }
+
+        Optional.ofNullable(rows)
+                .orElseGet(ArrayList::new)
+                .add(row);
+
+        session.setAttribute(SAVING_ATTRIBUTE_NAME, rows);
+    }
+}
