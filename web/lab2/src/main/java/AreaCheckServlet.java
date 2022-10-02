e9094d719f1c7a5b47f94c423c1656ba8223dab7
Arlet <shulga@niuitmo.ru>
2022-10-02 20:11:47 +0300

    Add servlet for check point hitting

---
+++ web/lab2/src/main/java/AreaCheckServlet.java
@@ -0,0 +1,43 @@
+import data.Row;
+import model.CoordinatesValidator;
+import model.DataSaver;
+import model.HitChecker;
+import model.RowsCreator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/area_checker")
+public class AreaCheckServlet extends HttpServlet {
+    private final CoordinatesValidator validator = new CoordinatesValidator();
+    private final HitChecker hitChecker = new HitChecker();
+
+    private final RowsCreator rowsCreator = new RowsCreator(validator, hitChecker);
+    private final DataSaver dataSaver = new DataSaver();
+
+    @Override
+    public void init() throws ServletException {
+        validator
+                .setMinX(2).setMaxX(2)
+                .setMinY(-3).setMaxY(3)
+                .setMinR(1).setMaxR(5);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String x = req.getParameter("x");
+        String y = req.getParameter("y");
+        String r = req.getParameter("r");
+
+        Row row = rowsCreator.createRow(x, y, r);
+
+        dataSaver.saveData(req.getSession(), row);
+
+        getServletContext().getRequestDispatcher("/table.jsp").forward(req, resp);
+    }
+
+}
