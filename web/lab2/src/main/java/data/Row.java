82a33e34882d99f19a1cf1fb69fdc69a9e778eef
Arlet <shulga@niuitmo.ru>
2022-10-02 16:34:28 +0300

    Create Row java-bean class for data saving

--- web/lab2/src/main/java/data/Row.java
+++ web/lab2/src/main/java/data/Row.java
@@ -1,16 +1,38 @@
 package data;
 
-public class Row {
-    private final String x;
-    private final String y;
-    private final String r;
+import java.io.Serializable;
 
-    private final String hitStatus;
+public class Row implements Serializable {
+    private String x;
+    private String y;
+    private String r;
+
+    private String hitStatus;
+
+    public Row() {
+
+    }
 
     public Row(String x, String y, String r, String hitStatus) {
         this.x = x;
         this.y = y;
         this.r = r;
+        this.hitStatus = hitStatus;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public void setR(String r) {
+        this.r = r;
+    }
+
+    public void setHitStatus(String hitStatus) {
         this.hitStatus = hitStatus;
     }
 
