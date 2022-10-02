a10ddb678c66b3e54eeaeecf146c6b48565ee19a
Arlet <shulga@niuitmo.ru>
2022-10-03 02:01:05 +0300

    Add servlet security reminder

--- web/lab2/src/main/java/AreaCheckServlet.java
+++ web/lab2/src/main/java/AreaCheckServlet.java
@@ -5,6 +5,7 @@ import model.HitChecker;
 import model.RowsCreator;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/area_checker")
+//todo: @ServletSecurity()
 public class AreaCheckServlet extends HttpServlet {
     private final CoordinatesValidator validator = new CoordinatesValidator();
     private final HitChecker hitChecker = new HitChecker();
