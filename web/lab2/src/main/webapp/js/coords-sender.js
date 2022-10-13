f20ce1214d83d3bf6702b99c9a73667e22da45f2
Arlet2 <shulga@niuitmo.ru>
2022-10-13 08:11:28 +0300

    Split map and table requests

--- web/lab2/src/main/webapp/js/coords-sender.js
+++ web/lab2/src/main/webapp/js/coords-sender.js
@@ -1,4 +1,4 @@
-function sendCoordinates() {
+function sendCoordinatesByForm() {
 
     let x = $('input[name="x"]:checked').val();
     let y = $('input[name="y"]').val();
@@ -23,44 +23,26 @@ function sendCoordinates() {
     y = clearSpacesAndChangeCommaToPoint(y);
     r = clearSpacesAndChangeCommaToPoint(r);
 
-    $.ajax({
-        type: "GET",
-        url: "controller",
-        data: {
-            'x': $('input[name="x"]:checked').val(),
-            'y': $('input[name="y"]').val(),
-            'r': $('input[name="r"]:checked').val(),
-            'mode': 0
-        },
-        cache: false,
-        dataType: "html",
-        beforeSend: function () {
-            if (!validate(x, y, r)) {
-                return false;
-            }
-        },
-        success: function (response) {
-            try {
-                handleRequest(response);
-            } catch (e) {
-                alert("Проблемы с ответом от сервера: " + e);
-            }
-        },
-        statusCode: {
-            404: function() {
-                alert("File not found.");
-            },
-            410: function() {
-                alert("Content was removed.");
-            },
-            500: function() {
-                alert("Server error");
-            },
-            502: function() {
-                alert("Bad gateway");
-            }
-        }
-    });
+    let coordinates = createCoordinates(
+        $('input[name="x"]:checked').val(),
+        $('input[name="y"]').val(),
+        $('input[name="r"]:checked').val()
+    );
+
+    ajaxSend(coordinates, 0, setResponseOnTable);
+}
+
+function sendCoordinatesByMap(x, y) {
+    let coordinates = createCoordinates(x, y, $('input[name="r"]:checked').val());
+    ajaxSend(coordinates, 1, setResponseOnMap);
+}
+
+function createCoordinates(x, y, r) {
+    return {
+            "x": x,
+            "y", y,
+            "r", r
+    };
 }
 
 function isXEmpty(x) {
@@ -79,10 +61,46 @@ function clearSpacesAndChangeCommaToPoint(sendingValue) {
     return sendingValue.replace(',', '.').replace(' ', "");
 }
 
-function createArgs(x, y, r) {
-    return "x=" + x + "&y=" + y + "&r=" + r;
+function ajaxSend(coordinates, mode, handleRequest) {
+    $.ajax({
+            type: "GET",
+            url: "controller",
+            data: {
+                'x': coordinates.x,
+                'y': coordinates.y,
+                'r': coordinates.r,
+                'mode': mode
+            },
+            cache: false,
+            dataType: "html",
+            success: function (response) {
+                try {
+                    handleRequest(response);
+                } catch (e) {
+                    alert("Проблемы с ответом от сервера: " + e);
+                }
+            },
+            statusCode: {
+                404: function() {
+                    alert("File not found.");
+                },
+                410: function() {
+                    alert("Content was removed.");
+                },
+                500: function() {
+                    alert("Server error");
+                },
+                502: function() {
+                    alert("Bad gateway");
+                }
+            }
+        });
 }
 
-function handleRequest(response) {
+function setResponseOnTable(response) {
     document.getElementById("receivingData").innerHTML = response;
+}
+
+function setResponseOnMap(response) {
+
 }
