22c8f4c272942db5bd5cd83e9cbc4b8760caf262
Arlet2 <shulga@niuitmo.ru>
2022-10-30 18:09:48 +0300

    Create filter for count headers

---
+++ web/lab2/src/main/java/filters/HeaderFilter.java
@@ -0,0 +1,40 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebFilter(filterName = "HeaderFilter")
+public class HeaderFilter implements Filter {
+    private Map<String, Long> headersMap;
+
+    public void init(FilterConfig config) throws ServletException {
+        headersMap = new HashMap<>();
+    }
+
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        Enumeration<String> headers = ((HttpServletRequest) request).getHeaderNames();
+
+        while (headers.hasMoreElements())
+            countHeader(headers.nextElement());
+
+        chain.doFilter(request, response);
+    }
+
+    private synchronized void countHeader(String headerName) {
+        if (!headersMap.containsKey(headerName))
+            headersMap.put(headerName, 1L);
+        else
+            headersMap.replace(headerName, headersMap.get(headerName)+1);
+    }
+}
