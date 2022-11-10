326a8c855d3c548a16197368cfe1198fdc17cd61
Arlet2 <shulga@niuitmo.ru>
2022-11-10 15:47:44 +0300

    Create converter and exceptions

---
+++ web/lab3/src/main/java/IncorrectPresentationException.java
@@ -0,0 +1,5 @@
+public class IncorrectPresentationException extends RuntimeException {
+    IncorrectPresentationException() {
+        super();
+    }
+}
---
+++ web/lab3/src/main/java/InvalidRangeException.java
@@ -0,0 +1,5 @@
+public class InvalidRangeException extends RuntimeException {
+    InvalidRangeException() {
+        super();
+    }
+}
