9fcf46245c86d1d634245975ded65f6a21b732ae
Arlet2 <shulga@niuitmo.ru>
2022-11-15 05:07:43 +0300

    Create scripts for handling map

--- web/lab3/src/main/webapp/js/form-handler.js
+++
@@ -1,6 +0,0 @@
-$('input[name="coordinatesForm:radius"]').attr("readonly", true);
-function isRCorrect() {
-    let r = $('input[name="coordinatesForm:radius"]').val();
-
-    return 1 <= r && r <= 4 && r%0.25==0;
-}
