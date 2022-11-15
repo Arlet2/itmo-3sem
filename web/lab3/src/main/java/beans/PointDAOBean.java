6e4a37bc8f1dd93db83db602ee7f149f61340659
Arlet2 <shulga@niuitmo.ru>
2022-11-15 04:49:02 +0300

    Create bean for databse handling

--- web/lab3/src/main/java/beans/PointDAOBean.java
+++ web/lab3/src/main/java/beans/PointDAOBean.java
@@ -1,20 +1,34 @@
 package beans;
 
+import database.AbstractPointDAO;
+import database.PointDAO;
 import lombok.Data;
+import database.Row;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
+import java.util.List;
 
 @Data
-@ManagedBean(name="database")
+@ManagedBean(name="points", eager = true)
 @ApplicationScoped
-public class DatabaseBean implements Serializable {
-    public DatabaseBean() {
+public class PointDAOBean implements Serializable {
+    private AbstractPointDAO pointDAO = new PointDAO();
+    private List<Row> pointsCollection;
 
+    public PointDAOBean() {
+        updatePointsCollection();
     }
 
-    public void test() {
-        
+    public void updatePointsCollection() {
+        pointsCollection = pointDAO.getPoints();
     }
+
+    public void addPoint(Row row) {
+        pointDAO.addPoint(row);
+
+        updatePointsCollection();
+    }
+
 }
