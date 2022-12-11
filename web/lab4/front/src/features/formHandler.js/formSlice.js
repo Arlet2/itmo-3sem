b73b4c359f2fc9c6136f37ce13c43d8b18e04495
Arlet2 <shulga@niuitmo.ru>
2022-12-11 22:44:51 +0300

    Create slice for form

---
+++ web/lab4/front/src/features/formHandler.js/formSlice.js
@@ -0,0 +1,37 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    x: undefined,
+    y: 5,
+    r: undefined
+};
+
+const formSlice = createSlice({
+    name: "formHandler",
+    initialState,
+    reducers: {
+        setX: (state, action) => {
+            if (state.x === action.payload)
+                state.x = undefined;
+            else
+                state.x = action.payload;
+        },
+        setY: (state, action) => {
+            state.y = action.payload;
+        },
+        setR: (state, action) => {
+            if (state.r === action.payload)
+                state.r = undefined;
+            else
+                state.r = action.payload;
+        }
+    }
+});
+
+export const { setX, setY, setR } = formSlice.actions;
+
+export const selectX = (state) => state.formHandler.x;
+export const selectY = (state) => state.formHandler.y;
+export const selectR = (state) => state.formHandler.r;
+
+export default formSlice.reducer;
