ce668f0cda5354cd96e2e60c7ab33773a5c4c5dc
Arlet2 <shulga@niuitmo.ru>
2022-12-12 16:16:33 +0300

    Add auth slice to store

--- web/lab4/front/src/app/store.js
+++ web/lab4/front/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '../features/auth/authSlice';
 import formSlice from '../features/formHandler.js/formSlice';
 
 export const store = configureStore({
 	reducer: {
-		formHandler: formSlice
+		formHandler: formSlice,
+		auth: authSlice
 	}
 });
