f07d63def2527807a129215d34e717d8bb8543ce
Arlet2 <shulga@niuitmo.ru>
2022-12-13 02:45:24 +0300

    Create prototype of points controller

---
+++ web/lab4/backend/src/main/java/com/arlet/lab4/controllers/PointsController.java
@@ -0,0 +1,10 @@
+package com.arlet.lab4.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="api/handler")
+public class PointsController {
+
+}
