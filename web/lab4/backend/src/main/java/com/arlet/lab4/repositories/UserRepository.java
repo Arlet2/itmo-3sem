70234aca0e0fc3853cdacb242d8c8d58409762db
Arlet2 <shulga@niuitmo.ru>
2022-12-13 02:43:40 +0300

    Create repository for users

---
+++ web/lab4/backend/src/main/java/com/arlet/lab4/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.arlet.lab4.repositories;
+
+import com.arlet.lab4.data.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsUserByLogin(String login);
+    User getUserByLogin(String login);
+}
