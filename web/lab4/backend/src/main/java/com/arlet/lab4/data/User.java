a932b88c9bcfd6fe15399f7cd774077d3aa2e24f
Arlet2 <shulga@niuitmo.ru>
2022-12-13 02:44:00 +0300

    Create jpa entity user

---
+++ web/lab4/backend/src/main/java/com/arlet/lab4/data/User.java
@@ -0,0 +1,15 @@
+package com.arlet.lab4.data;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+@Data
+public class User {
+    @Id
+    private String login;
+    private String password;
+    private String salt;
+}
