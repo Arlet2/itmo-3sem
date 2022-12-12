73a355ca81f732be36abd72d8415b542f236f238
Arlet2 <shulga@niuitmo.ru>
2022-12-12 16:18:11 +0300

    Create prototype of login

--- web/lab4/front/src/components/Login.js
+++ web/lab4/front/src/components/Login.js
@@ -4,19 +4,75 @@ import Card from "react-toolbox/lib/card/Card";
 import CardTitle from "react-toolbox/lib/card/CardTitle";
 import CardActions from "react-toolbox/lib/card/CardActions";
 import CardText from "react-toolbox/lib/card/CardText";
+import Input from "react-toolbox/lib/input/Input";
+import Button from "react-toolbox/lib/button/Button";
+
+import '../assets/react-toolbox/theme.css';
+import theme from '../assets/react-toolbox/theme.js';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import Cookies from "js-cookie";
+import {jQuery as $} from "jquery";
 
 function Login() {
+    checkToken();
     return (
-        <div>
-            <Card>
-                <CardTitle title="Authorization"/>
-
-                <CardText>
-                    <input type="button"/>
-                </CardText>
-            </Card>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div>
+                <Card>
+                    <CardTitle>Authorization</CardTitle>
+                    <CardText>
+                        <Input label="Login"/>
+                        <Input type="password" label="Password"/>
+                    </CardText>
+                    <CardActions>
+                        <Button label="Login" onClick={getTokenByLogin}/>
+                        <Button label="Register" onClick={getTokenByRegister}/>
+                    </CardActions>
+                </Card>
+            </div>
+        </ThemeProvider>
     );
+}
+
+function checkToken() {
+    if (Cookies.get("jwt-token")) {
+        if (!document.referrer.includes("login"))
+            returnBack();
+    }
+}
+
+function returnBack() {
+    window.location.replace(document.referrer);
+}
+
+function getTokenByLogin() {
+    Cookies.set("jwt-token", "1235");
+    returnBack();
+    return;
+    $.ajax({
+        type: "POST",
+        url: "/api/login",
+        data: {},
+        dataType: "json",
+        success: function (response) {
+            
+        }
+    });
+    
+}
+function getTokenByRegister() {
+    Cookies.set("jwt-token", "1234");
+    returnBack();
+
+    $.ajax({
+        type: "POST",
+        url: "/api/register",
+        data: {},
+        dataType: "json",
+        success: function (response) {
+            
+        }
+    });
 }
 
 export default Login;
