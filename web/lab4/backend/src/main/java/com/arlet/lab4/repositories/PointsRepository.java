2df6a325d40568c447c512c679bd75ad35e3597a
Arlet2 <shulga@niuitmo.ru>
2022-12-13 13:55:34 +0300

    Add find by owner

--- web/lab4/backend/src/main/java/com/arlet/lab4/repositories/PointsRepository.java
+++ web/lab4/backend/src/main/java/com/arlet/lab4/repositories/PointsRepository.java
@@ -3,5 +3,8 @@ package com.arlet.lab4.repositories;
 import com.arlet.lab4.data.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PointsRepository extends JpaRepository<Point, Long> {
+    List<Point> findAllByOwner(String owner);
 }
