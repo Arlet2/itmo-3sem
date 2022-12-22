371dc4c5c5c163b7408cfad360cc6446e54b6e2a
Arlet2 <shulga@niuitmo.ru>
2022-12-22 10:55:32 +0300

    Add configuration for filters

---
+++ web/lab4/backend/src/main/java/com/arlet/lab4/FiltersConfiguration.java
@@ -0,0 +1,38 @@
+package com.arlet.lab4;
+
+import com.arlet.lab4.filters.APIFilter;
+import com.arlet.lab4.filters.SecureFilter;
+import com.arlet.lab4.services.AuthService;
+import com.arlet.lab4.services.CookiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FiltersConfiguration {
+    @Bean
+    public FilterRegistrationBean<SecureFilter> createSecureFilter(
+            @Autowired AuthService authService, @Autowired CookiesService cookiesService) {
+        var filter = new FilterRegistrationBean<SecureFilter>();
+
+        filter.setFilter(new SecureFilter(authService, cookiesService));
+
+        filter.addUrlPatterns("/api/secure/*");
+
+        return filter;
+    }
+
+    @Bean
+    public FilterRegistrationBean<APIFilter> createFilterAPIFilter() {
+        var filter = new FilterRegistrationBean<APIFilter>();
+
+        filter.setFilter(new APIFilter());
+
+        filter.addUrlPatterns("/api/*");
+
+        filter.setOrder(0);
+
+        return filter;
+    }
+}
