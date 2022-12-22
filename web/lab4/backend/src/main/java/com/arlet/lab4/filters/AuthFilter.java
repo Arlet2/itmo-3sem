1c7757a27b39f0bbdf1b5236b1e167ac09506a28
Arlet2 <shulga@niuitmo.ru>
2022-12-22 10:34:19 +0300

    Fix auth filter and add error sending

--- web/lab4/backend/src/main/java/com/arlet/lab4/filters/AuthFilter.java
+++ web/lab4/backend/src/main/java/com/arlet/lab4/filters/AuthFilter.java
@@ -3,19 +3,17 @@ package com.arlet.lab4.filters;
 import com.arlet.lab4.services.AuthService;
 import com.arlet.lab4.services.CookiesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
 @Component
-@WebFilter(urlPatterns = "/api/handler")
-@Order(1)
-public class AuthFilter implements Filter {
+public class AuthFilter extends GenericFilterBean {
 
     private final AuthService authService;
     private final CookiesService cookiesService;
@@ -29,13 +27,24 @@ public class AuthFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
-        Optional<String> jwt = cookiesService.getJWTFromCookie((HttpServletRequest) servletRequest);
+        System.out.println("AUTH FILTER CALLED");
 
-        if (jwt.isEmpty())
-            return;
+        var request = (HttpServletRequest) servletRequest;
+        var response = (HttpServletResponse) servletResponse;
 
-        if(!authService.isJWTValid(jwt.get()))
+        Optional<String> jwt = cookiesService.getJWTFromCookie(request);
+
+        if (jwt.isEmpty()) {
+            System.out.println("No jwt request");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
+        }
+
+        if(!authService.isJWTValid(jwt.get())) {
+            System.out.println("Request with invalid jwt");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
