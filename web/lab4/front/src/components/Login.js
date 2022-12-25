16d26b58681c8bf75221ea40e2ab263e2f027b24
Arlet2 <shulga@niuitmo.ru>
2022-12-26 02:43:58 +0300

    Finalize login and register

--- web/lab4/front/src/components/Login.js
+++ web/lab4/front/src/components/Login.js
@@ -4,30 +4,135 @@ import Card from "react-toolbox/lib/card/Card";
 import CardTitle from "react-toolbox/lib/card/CardTitle";
 import CardActions from "react-toolbox/lib/card/CardActions";
 import CardText from "react-toolbox/lib/card/CardText";
-import Input from "react-toolbox/lib/input/Input";
 import Button from "react-toolbox/lib/button/Button";
 
 import '../assets/react-toolbox/theme.css';
 import theme from '../assets/react-toolbox/theme.js';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import Cookies from "js-cookie";
-import {jQuery as $} from "jquery";
+import AuthField from "./AuthField";
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrorMessage, selectErrorMessage, selectIsLogin, selectLogin, selectPassword, selectRepeatedPassword, setErrorMessage, switchIsLogin } from "../features/auth/authSlice";
+import { goToMain, returnBack, validateToken } from "../utils";
 
 function Login() {
     checkToken();
+
+    const isLogin = useSelector(selectIsLogin);
+    const login = useSelector(selectLogin);
+    const password = useSelector(selectPassword);
+    const repeatedPassword = useSelector(selectRepeatedPassword);
+    const errorMessage = useSelector(selectErrorMessage);
+
+    let buttonInfo = { name: undefined, action: undefined };
+    let switchButtonInfo = undefined;
+
+    const dispatch = useDispatch();
+
+    const validateFields = () => {
+        if (login === "" || login === undefined) {
+            dispatch(setErrorMessage("Логин не может быть пустым"));
+            return false;
+        }
+
+        if (password === "" || password === undefined) {
+            dispatch(setErrorMessage("Пароль не может быть пустым"));
+            return false;
+        }
+
+        if (password.length < 6) {
+            dispatch(setErrorMessage("Пароль должен состоять из не менее 6 символов"));
+            return false;
+        }
+
+        return true;
+    };
+
+    const getTokenByLogin = () => {
+        if (!validateFields())
+            return;
+
+        clearErrorMessage();
+
+        fetch("http://localhost:8080/api/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({login: login, password: password})
+        })
+            .then(
+                (result) => {
+                    if (result.ok) {
+
+                        result.text().then(
+                            (text) => { Cookies.set("jwt-token", text); }
+                        );
+
+                        returnBack();
+                    }
+                    else {
+                        result.text().then(
+                            (text) => { dispatch(setErrorMessage(JSON.parse(text).message)) }
+                        );
+                    }
+                });
+    };
+
+    const getTokenByRegister = () => {
+        if (!validateFields())
+            return;
+
+        if (password !== repeatedPassword) {
+            dispatch(setErrorMessage("Пароли должны быть одинаковы"));
+            return;
+        }
+
+        clearErrorMessage();
+
+        fetch("http://localhost:8080/api/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ login: login, password: password })
+        })
+            .then(
+                (result) => {
+                    if (result.ok) {
+
+                        result.text().then(
+                            (text) => { Cookies.set("jwt-token", text); }
+                        );
+
+                        returnBack();
+                    }
+                    else {
+                        result.text().then(
+                            (text) => { dispatch(setErrorMessage(JSON.parse(text).message)) }
+                        );
+                    }
+                });
+    };
+
+    if (isLogin) {
+        buttonInfo = { name: "Sign in", action: getTokenByLogin };
+        switchButtonInfo = "Switch to register";
+    }
+    else {
+        buttonInfo = { name: "Sign up", action: getTokenByRegister };
+        switchButtonInfo = "Switch to log in";
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <div>
                 <Card>
                     <CardTitle>Authorization</CardTitle>
                     <CardText>
-                        <Input label="Login"/>
-                        <Input type="password" label="Password"/>
+                        <AuthField />
                     </CardText>
                     <CardActions>
-                        <Button label="Login" onClick={getTokenByLogin}/>
-                        <Button label="Register" onClick={getTokenByRegister}/>
+                        <Button label={buttonInfo.name} onClick={() => buttonInfo.action()} />
+                        <Button label={switchButtonInfo} onClick={() => dispatch(switchIsLogin())} />
                     </CardActions>
+                    <p>{errorMessage}</p>
                 </Card>
             </div>
         </ThemeProvider>
@@ -35,44 +140,18 @@ function Login() {
 }
 
 function checkToken() {
-    if (Cookies.get("jwt-token")) {
-        if (!document.referrer.includes("login"))
-            returnBack();
-    }
-}
-
-function returnBack() {
-    window.location.replace(document.referrer);
-}
-
-function getTokenByLogin() {
-    Cookies.set("jwt-token", "1235");
-    returnBack();
-    return;
-    $.ajax({
-        type: "POST",
-        url: "/api/login",
-        data: {},
-        dataType: "json",
-        success: function (response) {
-            
+    validateToken().then(
+        (result) => {
+            if (result) {
+                if (!document.referrer.includes("login"))
+                    returnBack();
+                else
+                    goToMain();
+            } else {
+                Cookies.remove("jwt-token");
+            }
         }
-    });
-    
-}
-function getTokenByRegister() {
-    Cookies.set("jwt-token", "1234");
-    returnBack();
-
-    $.ajax({
-        type: "POST",
-        url: "/api/register",
-        data: {},
-        dataType: "json",
-        success: function (response) {
-            
-        }
-    });
+    );
 }
 
 export default Login;
