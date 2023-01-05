658958a3661ec94365ab959ba568fe8d68519bac
Arlet2 <shulga@niuitmo.ru>
2023-01-05 18:27:45 +0300

    Remove extra filter configuration

--- web/lab4/backend/src/main/java/com/arlet/lab4/configurations/FiltersConfiguration.java
+++ web/lab4/backend/src/main/java/com/arlet/lab4/configurations/FiltersConfiguration.java
@@ -1,11 +1,9 @@
 package com.arlet.lab4.configurations;
 
-import com.arlet.lab4.filters.CORSFilter;
 import com.arlet.lab4.filters.SecureFilter;
 import com.arlet.lab4.services.AuthService;
 import com.arlet.lab4.services.CookiesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,20 +18,6 @@ public class FiltersConfiguration {
         filter.setFilter(new SecureFilter(authService, cookiesService));
 
         filter.addUrlPatterns("/api/secure/*");
-
-        return filter;
-    }
-
-    @Bean
-    public FilterRegistrationBean<CORSFilter> createFilterAPIFilter(
-            @Value("${allowed_api_origins}") String[] allowedOrigins) {
-        var filter = new FilterRegistrationBean<CORSFilter>();
-
-        filter.setFilter(new CORSFilter(allowedOrigins));
-
-        filter.addUrlPatterns("/api/*");
-
-        filter.setOrder(0);
 
         return filter;
     }
